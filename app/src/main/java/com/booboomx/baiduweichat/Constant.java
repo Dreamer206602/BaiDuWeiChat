58523644fff7bd9b8824ae74ec161a3d89a9a58f
Dreamer206602 <boobooMX@163.com>
2017-01-11 08:49:20 +0800

    Initial commit

---
+++ app/src/main/java/com/booboomx/baiduweichat/Constant.java
@@ -0,0 +1,10 @@
+package com.booboomx.baiduweichat;
+
+/**
+ * Created by booboomx on 17/1/10.
+ */
+
+public class Constant {
+    public static final int REQUEST_CODE=0X0001;
+    public static final int RESULT_CODE=0X0001;
+}
---
+++ app/src/main/java/com/booboomx/baiduweichat/MainActivity.java
@@ -0,0 +1,56 @@
+package com.booboomx.baiduweichat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.booboomx.baiduweichat.ui.activity.LocationActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    //          SHA1: 1E:5F:31:A5:ED:1E:76:78:CD:0A:B0:D2:46:C9:5C:C7:8F:80:10:BC
+
+    //  keytool -v  -list -keystore /Users/booboomx/Android/As_WorkSpace/BaiDuWeiChat/baiduwechat.jks
+
+    private Button mBtnLocation;
+    private TextView mTvLocation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mBtnLocation= (Button) findViewById(R.id.btn_location);
+        mTvLocation= (TextView) findViewById(R.id.tv_content);
+
+        mBtnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(MainActivity.this, LocationActivity.class);
+                startActivityForResult(intent,Constant.REQUEST_CODE);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case Constant.REQUEST_CODE:
+                if(resultCode==Constant.RESULT_CODE){
+
+                    String position=data.getStringExtra("position");
+                    mTvLocation.setText(position);
+
+                }
+                break;
+        }
+    }
+}
